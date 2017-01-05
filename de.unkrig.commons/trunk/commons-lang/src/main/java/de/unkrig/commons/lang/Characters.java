5653fab41c4aa886970390e2f03f1c4894508b19
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2017-01-05 11:10:57 +0000

    Changed character class predicates from Predicate<Character> to Predicate<Integer> to accomodate for UNICODE supplementary characters (those with conde points >= 0x10000). Fixed a bug in "IS_POSIX_SPACE". JAVADOC.

--- de.unkrig.commons/trunk/commons-lang/src/main/java/de/unkrig/commons/lang/Characters.java
+++ de.unkrig.commons/trunk/commons-lang/src/main/java/de/unkrig/commons/lang/Characters.java
@@ -29,97 +29,151 @@ package de.unkrig.commons.lang;
 import de.unkrig.commons.lang.protocol.Predicate;
 
 /**
- * Extensions for {@link Character}.
+ * Extensions for the JRE's {@link Character} class.
  */
 public final
 class Characters {
 
     private Characters() {}
 
-    public static final Predicate<Character>
-    IS_POSIX_LOWER = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "lower" ({@code [a-z]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_LOWER = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return c >= 'a' && c <= 'z';
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_UPPER = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "upper" ({@code [A-Z]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_UPPER = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return c >= 'A' && c <= 'Z';
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_ASCII = new Predicate<Character>() {
-        @Override public boolean evaluate(Character subject) { return subject <= 0x7f; }
+    /**
+     * Evaluates whether a given code point is in the ASCII range (0-127).
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_ASCII = new Predicate<Integer>() {
+        @Override public boolean evaluate(Integer subject) { return subject <= 0x7f; }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_ALPHA = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "alpha" ({@code [A-Za-z]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_ALPHA = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_DIGIT = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "digit" ({@code [0-9]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_DIGIT = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return c >= '0' && c <= '9';
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_ALNUM = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "alnum" ({@code [A-Za-z0-9]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_ALNUM = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_PUNCT = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "punct" (one of <code>! " # $ % &amp;
+     * ' ( ) * + , - . / : ; &lt; = > ? @ [ \ ] ^ _ ` { | } ~</code>).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_PUNCT = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return (
-                (c >= '!' && c <= '/')    // !"#$%&'()*+,-./
-                || (c >= ':' && c <= '@') // :;<=>?@
-                || (c >= '[' && c <= '`') // [\]^_`
-                || (c >= '{' && c <= '~') // {|}~
+                (c >= '!' && c <= '/')    // !"#$%&'()*+,-./ (33...47)
+                || (c >= ':' && c <= '@') // :;<=>?@         (58...64)
+                || (c >= '[' && c <= '`') // [\]^_`          (91...96)
+                || (c >= '{' && c <= '~') // {|}~            (123...126)
             );
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_GRAPH = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "graph"; the union of classes "alpha",
+     * "digit", and "punct".
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_GRAPH = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
+        evaluate(Integer subject) {
             return Characters.IS_POSIX_ALNUM.evaluate(subject) || Characters.IS_POSIX_PUNCT.evaluate(subject);
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_PRINT = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "print"; the union of classes "alpha",
+     * "digit" and "punct", and the SPACE character.
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_PRINT = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
+        evaluate(Integer subject) {
             return (
                 subject == ' '
                 || Characters.IS_POSIX_ALNUM.evaluate(subject)
@@ -128,63 +182,96 @@ class Characters {
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_BLANK = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "blank"; which consists of the SPACE
+     * character and the TAB character.
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_BLANK = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return c == ' ' || c == '\t';
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_CNTRL = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "cntrl" ({@code [\0-\x1f\x7f]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_CNTRL = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return c <= 0x1f || c == 0x7f;
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_XDIGIT = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "xdigit" ({@code [0-9a-fA-F]}).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_XDIGIT = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
+        evaluate(Integer subject) {
+            int c = subject;
             return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
         }
     };
 
-    public static final Predicate<Character>
-    IS_POSIX_SPACE = new Predicate<Character>() {
+    /**
+     * Evaluates whether a given code point lies in the POSIX character class "space" (consisting of the tab, newline,
+     * vertical-tab, form-feed, carriage-return and space characters).
+     *
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap07.html#tag_07_03_01">The Open
+     *      Group Base Specifications Issue 7, section 7.3.1: LC_CTYPE</a>
+     */
+    public static final Predicate<Integer>
+    IS_POSIX_SPACE = new Predicate<Integer>() {
 
         @Override public boolean
-        evaluate(Character subject) {
-            char c = subject;
-            return c >= 'a' && c <= 'z';
+        evaluate(Integer subject) {
+            int c = subject;
+            return (
+                (c >= 9 && c <= 13) // 0x09=tab, 0x0a=newline, 0x0b=vertical-tab, 0x0c=form-feed, 0x0d=carriage-return
+                || c == ' '         // 0x20=space
+            );
         }
     };
 
-    public static final Predicate<Character>
-    IS_LOWER_CASE = new Predicate<Character>() {
-        @Override public boolean evaluate(Character subject) { return Character.isLowerCase(subject); }
+    /** A predicate for {@link Character#isLowerCase(int)}. */
+    public static final Predicate<Integer>
+    IS_LOWER_CASE = new Predicate<Integer>() {
+        @Override public boolean evaluate(Integer subject) { return Character.isLowerCase(subject); }
     };
 
-    public static final Predicate<Character>
-    IS_UPPER_CASE = new Predicate<Character>() {
-        @Override public boolean evaluate(Character subject) { return Character.isUpperCase(subject); }
+    /** A predicate for {@link Character#isUpperCase(int)}. */
+    public static final Predicate<Integer>
+    IS_UPPER_CASE = new Predicate<Integer>() {
+        @Override public boolean evaluate(Integer subject) { return Character.isUpperCase(subject); }
     };
 
-    public static final Predicate<Character>
-    IS_WHITESPACE = new Predicate<Character>() {
-        @Override public boolean evaluate(Character subject) { return Character.isWhitespace(subject); }
+    /** A predicate for {@link Character#isWhitespace(int)}. */
+    public static final Predicate<Integer>
+    IS_WHITESPACE = new Predicate<Integer>() {
+        @Override public boolean evaluate(Integer subject) { return Character.isWhitespace(subject); }
     };
 
-    public static final Predicate<Character>
-    IS_MIRRORED = new Predicate<Character>() {
-        @Override public boolean evaluate(Character subject) { return Character.isMirrored(subject); }
+    /** A predicate for {@link Character#isMirrored(int)}. */
+    public static final Predicate<Integer>
+    IS_MIRRORED = new Predicate<Integer>() {
+        @Override public boolean evaluate(Integer subject) { return Character.isMirrored(subject); }
     };
 }
