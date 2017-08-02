567a41a0c5ff3b04d7b8a689efc6967914ac1a39
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2017-08-02 15:07:59 +0000

    Extended the "StringUtil.IndexOf" interface with various "lastIndexOf()" methods.

--- de.unkrig.commons/trunk/commons-lang/src/test/java/test/StringUtilTest.java
+++ de.unkrig.commons/trunk/commons-lang/src/test/java/test/StringUtilTest.java
@@ -31,39 +31,88 @@ import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
 
+import de.unkrig.commons.lang.AssertionUtil;
 import de.unkrig.commons.lang.StringUtil;
 import de.unkrig.commons.lang.StringUtil.IndexOf;
+import de.unkrig.commons.lang.protocol.Producer;
 
 // CHECKSTYLE Javadoc:OFF
 
 public
 class StringUtilTest {
 
+    static { AssertionUtil.enableAssertionsForThisClass(); }
+
     @Test public void
-    testIndexOf() {
+    testNewNaiveIndexOf() {
 
         String infix = "ABCDEFGHIJKLMNOP";
-        Random r     = new Random(123);
 
-        // Compose a LONG subject string.
-        StringBuilder subject = new StringBuilder();
-        for (int i = 0; i < 20; i++) {
-            for (int j = r.nextInt(64) - 32; j > 0; j--) subject.append('X');
-            subject.append(infix, 0, r.nextInt(infix.length() + 1));
-            for (int j = r.nextInt(64) - 32; j > 0; j--) subject.append('X');
-            subject.append(infix, r.nextInt(infix.length()), infix.length());
+        IndexOf io = StringUtil.newNaiveIndexOf(infix);
+
+        Producer<String> rsp = StringUtilTest.randomSubjectProducer(infix);
+
+        for (int j = 0; j < 100000; j++) {
+            String subject = AssertionUtil.notNull(rsp.produce());
+            for (int offset = 0;; offset += subject.length()) {
+
+                int o1 = subject.indexOf(infix, offset);
+                int o2 = io.indexOf(subject, offset, subject.length());
+
+                Assert.assertEquals(o1, o2);
+
+                if (o1 == -1) break;
+            }
         }
+    }
 
-        IndexOf io = StringUtil.newIndexOf(infix);
+    @Test public void
+    testNewKnuthMorrisPrattIndexOf() {
 
-        for (int offset = 0;; offset += subject.length()) {
+        String infix = "ABCDEFGHIJKLMNOP";
 
-            int o1 = subject.indexOf(infix, offset);
-            int o2 = io.indexOf(subject, offset, subject.length());
+        IndexOf io = StringUtil.newKnuthMorrisPrattIndexOf(infix);
 
-            Assert.assertEquals(o1, o2);
+        Producer<String> rsp = StringUtilTest.randomSubjectProducer(infix);
 
-            if (o1 == -1) break;
+        for (int j = 0; j < 100000; j++) {
+            String subject = AssertionUtil.notNull(rsp.produce());
+            for (int offset = 0;; offset += subject.length()) {
+
+                int o1 = subject.indexOf(infix, offset);
+                int o2 = io.indexOf(subject, offset, subject.length());
+
+                if (o1 != o2) {
+                    o2 = io.indexOf(subject, offset, subject.length());
+                    System.currentTimeMillis();
+                }
+                Assert.assertEquals("Subject=\"" + subject + "\"", o1, o2);
+
+                if (o1 == -1) break;
+            }
         }
+    }
+
+    private static Producer<String>
+    randomSubjectProducer(final String infix) {
+
+        return new Producer<String>() {
+
+            Random r = new Random(123);
+
+            @Override public String
+            produce() {
+
+                StringBuilder subject = new StringBuilder();
+                for (int i = 0; i < 20; i++) {
+                    for (int j = this.r.nextInt(64) - 32; j > 0; j--) subject.append('X');
+                    subject.append(infix, 0, this.r.nextInt(infix.length() + 1));
+                    for (int j = this.r.nextInt(64) - 32; j > 0; j--) subject.append('X');
+                    subject.append(infix, this.r.nextInt(infix.length()), infix.length());
+                }
+
+                return subject.toString();
+            }
+        };
     }
 }
