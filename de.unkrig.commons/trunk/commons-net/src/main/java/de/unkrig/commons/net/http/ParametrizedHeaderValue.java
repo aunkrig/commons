fc62c30fb32da9f3f5006a2de94d88ccc5ff59a8
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2017-12-28 10:40:55 +0000

    Added "removeParameter()".

--- de.unkrig.commons/trunk/commons-net/src/main/java/de/unkrig/commons/net/http/ParametrizedHeaderValue.java
+++ de.unkrig.commons/trunk/commons-net/src/main/java/de/unkrig/commons/net/http/ParametrizedHeaderValue.java
@@ -29,40 +29,60 @@ package de.unkrig.commons.net.http;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.StringTokenizer;
 
 import de.unkrig.commons.nullanalysis.Nullable;
 
 /**
- * Parser for a 'parametrized value' of an HTTP header like:
+ * Parser for a "parametrized value" as defined in several HTTP-related RFCs.
+ * <ul>
+ *   <li><a href="https://tools.ietf.org/html/rfc2616#section-3.7">RFC2616, 3.7 Media Types</></li>
+ *   <li><a href="https://tools.ietf.org/html/rfc6265#section-4.1">RFC6265, 4.1 Set-Cookie</></li>
+ * </ul>
+ * <p>
+ *   Examples:
+ * </p>
  * <pre>
  * Content-Type: text/plain; charset=ASCII
+ * Set-Cookie: __VCAP_ID__=a8f33cb4-bdc9-4cef-7679-15212b52907c; Path=/; HttpOnly; Secure
  * </pre>
- * 'text/plain' is the so-called 'token', 'char=ASCII' is a so-called 'parameter'.
+ * <p>
+ *   "text/plain" is the {@link #getToken() token}, "char=ASCII" is a {@link #getParameter(String) parameter}
+ *   (which is sometimes, e.g. in the context of RFC6265, called an "attribute"), and "HttpOnly" is a parameter
+ *   with an empty value ({@code ""}).
+ * </p>
  */
 public
 class ParametrizedHeaderValue {
     private final String token;
 
-    /** upper-cased-name => value */
+    /** lower-cased-name => value */
     private final Map<String, String> parameters = new HashMap<String, String>();
 
     public
     ParametrizedHeaderValue(String s) {
-        int idx = s.indexOf(';');
-        if (idx == -1) {
-            this.token = s.trim();
-            return;
-        }
 
-        this.token = s.substring(0, idx).trim();
-        for (;;) {
-            int idx2 = s.indexOf('=', idx + 1);
-            if (idx2 == -1) break;
-            int idx3 = s.indexOf(';', idx2 + 1);
-            if (idx3 == -1) idx3 = s.length();
-            this.parameters.put(s.substring(idx + 1, idx2).trim().toUpperCase(), s.substring(idx2 + 1, idx3).trim());
-            if (idx3 == s.length()) break;
-            idx = idx3;
+        StringTokenizer st = new StringTokenizer(s, ";");
+
+        this.token = st.nextToken().trim();
+
+        while (st.hasMoreTokens()) {
+
+            String t = st.nextToken();
+
+            String parameterName, parameterValue;
+            {
+                int idx = t.indexOf('=');
+                if (idx == -1) {
+                    parameterName  = t.trim();
+                    parameterValue = "";
+                } else {
+                    parameterName  = t.substring(0, idx).trim();
+                    parameterValue = t.substring(idx + 1).trim();
+                }
+            }
+
+            this.parameters.put(parameterName.toLowerCase(), parameterValue);
         }
     }
 
@@ -74,23 +94,34 @@ class ParametrizedHeaderValue {
 
     /**
      * @param name The (case-insensitive) parameter name
-     * @return     The value of the named parameter, or {@code null} iff a paramater with that name does not exist
+     * @return     The value of the named parameter, or {@code null} iff a parameter with that name does not exist
      * @see        ParametrizedHeaderValue
      */
     @Nullable public String
     getParameter(String name) {
-        return this.parameters.get(name.toUpperCase());
+        return this.parameters.get(name.toLowerCase());
     }
 
     /**
      * Adds the given parameter, or changes the value of an existing parameter with that <var>name</var>.
      *
      * @return The previous value of the named parameter, or {@code null} iff a parameter with that <var>name</var> did
-     *         not exist before
+     *         not exist
      */
-    public String
+    @Nullable public String
     setParameter(String name, String value) {
-        return this.parameters.put(name.toUpperCase(), value);
+        return this.parameters.put(name.toLowerCase(), value);
+    }
+
+    /**
+     * Removes the given parameter if it exists.
+     *
+     * @return The previous value of the named parameter, or {@code null} iff a parameter with that <var>name</var> did
+     *         not exist
+     */
+    @Nullable public String
+    removeParameter(String name) {
+        return this.parameters.remove(name.toLowerCase());
     }
 
     @Override public String
@@ -98,7 +129,11 @@ class ParametrizedHeaderValue {
         if (this.parameters.isEmpty()) return this.token;
         StringBuilder sb = new StringBuilder(this.token);
         for (Entry<String, String> entry : this.parameters.entrySet()) {
-            sb.append("; ").append(entry.getKey().toLowerCase()).append("=").append(entry.getValue());
+            String parameterName  = entry.getKey();
+            String parameterValue = entry.getValue();
+
+            sb.append("; ").append(parameterName);
+            if (!parameterValue.isEmpty()) sb.append("=").append(parameterValue);
         }
         return sb.toString();
     }
