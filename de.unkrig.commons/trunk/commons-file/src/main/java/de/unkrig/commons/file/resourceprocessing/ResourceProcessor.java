65e648b5d6deb9a17e1e5abba85de1d08dd47ba5
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2019-02-22 14:45:00 +0000

    Added "de.unkrig.commons.file.resourceprocessing", in symmetry with "fileprocessing" and "contentsprocessing", to process *resources* (designated by URLs) in addition to files and InputStreams.

---
+++ de.unkrig.commons/trunk/commons-file/src/main/java/de/unkrig/commons/file/resourceprocessing/ResourceProcessor.java
@@ -0,0 +1,54 @@
+
+/*
+ * de.unkrig.commons - A general-purpose Java class library
+ *
+ * Copyright (c) 2011, Arno Unkrig
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *       following disclaimer in the documentation and/or other materials provided with the distribution.
+ *    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.unkrig.commons.file.resourceprocessing;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import de.unkrig.commons.lang.protocol.ProducerWhichThrows;
+import de.unkrig.commons.nullanalysis.Nullable;
+
+/**
+ * @param <T> The type produced by {@link #process(String, InputStream, long, long, ProducerWhichThrows)}
+ * @see       #process(String, InputStream, long, long, ProducerWhichThrows)
+ */
+public
+interface ResourceProcessor<T> {
+
+    /**
+     * Reads from the resource (typically until end-of-input).
+     *
+     * @param  path                 E.g. {@code "/dir/zipfile!dir/zipfile!dir/file"}
+     * @param  location             Designates the location of the resource
+     * @throws IOException          Message should NOT include the <var>path</var>
+     * @throws InterruptedException The processing was parallelized in multiple threads, and at least one of the
+     *                              threads was interrupted
+     */
+    @Nullable T
+    process(String path, URL location) throws IOException, InterruptedException;
+}
