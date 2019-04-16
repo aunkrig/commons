bf3b77f8bb086058a0f944bb4fe31dd2c84fa889
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2019-04-16 17:44:25 +0000

    Added "Tuple2" and "Tuple3".

---
+++ de.unkrig.commons/trunk/commons-lang/src/main/java/de/unkrig/commons/lang/protocol/Tuple2.java
@@ -0,0 +1,58 @@
+
+/*
+ * de.unkrig.commons - A general-purpose Java class library
+ *
+ * Copyright (c) 2019, Arno Unkrig
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *       following disclaimer in the documentation and/or other materials provided with the distribution.
+ *    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.unkrig.commons.lang.protocol;
+
+/**
+ * A container for two elements
+ *
+ * @param <T1> Type of the first element
+ * @param <T2> Type of the second element
+ */
+public
+class Tuple2<T1, T2> {
+
+    /**
+     * The first element of the tuple.
+     */
+    public final T1 first;
+
+    /**
+     * The second element of the tuple.
+     */
+    public final T2 second;
+
+    public
+    Tuple2(T1 first, T2 second) {
+        this.first  = first;
+        this.second = second;
+    }
+
+    @Override public String
+    toString() {
+        return this.first + ":" + this.second;
+    }
+}
---
+++ de.unkrig.commons/trunk/commons-lang/src/main/java/de/unkrig/commons/lang/protocol/Tuple3.java
@@ -0,0 +1,65 @@
+
+/*
+ * de.unkrig.commons - A general-purpose Java class library
+ *
+ * Copyright (c) 2019, Arno Unkrig
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *       following disclaimer in the documentation and/or other materials provided with the distribution.
+ *    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.unkrig.commons.lang.protocol;
+
+/**
+ * A container for three elements
+ *
+ * @param <T1> Type of the first element
+ * @param <T2> Type of the second element
+ * @param <T3> Type of the third element
+ */
+public
+class Tuple3<T1, T2, T3> {
+
+    /**
+     * The first element of the tuple.
+     */
+    public final T1 first;
+
+    /**
+     * The second element of the tuple.
+     */
+    public final T2 second;
+
+    /**
+     * The third element of the tuple.
+     */
+    public final T3 third;
+
+    public
+    Tuple3(T1 first, T2 second, T3 third) {
+        this.first  = first;
+        this.second = second;
+        this.third  = third;
+    }
+
+    @Override public String
+    toString() {
+        return this.first + ":" + this.second + ":" + this.third;
+    }
+}
