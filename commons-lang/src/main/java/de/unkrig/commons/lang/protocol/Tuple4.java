b9c3c1baf71c89248a0e9c03f608e705e893073a
Arno Unkrig <aunkrig@users.noreply.github.com>
2019-04-28 16:35:02 +0000

    Added "Tuple4".

--- commons-lang/src/main/java/de/unkrig/commons/lang/protocol/Tuple4.java
+++ commons-lang/src/main/java/de/unkrig/commons/lang/protocol/Tuple4.java
@@ -30,13 +30,15 @@ import de.unkrig.commons.lang.ObjectUtil;
 import de.unkrig.commons.nullanalysis.Nullable;
 
 /**
- * A container for two elements
+ * A container for four elements.
  *
  * @param <T1> Type of the first element
  * @param <T2> Type of the second element
+ * @param <T3> Type of the third element
+ * @param <T4> Type of the fourth element
  */
 public final
-class Tuple2<T1, T2> {
+class Tuple4<T1, T2, T3, T4> {
 
     /**
      * The first element of the tuple.
@@ -48,24 +50,48 @@ class Tuple2<T1, T2> {
      */
     public final T2 second;
 
+    /**
+     * The third element of the tuple.
+     */
+    public final T3 third;
+
+    /**
+     * The third element of the tuple.
+     */
+    public final T4 fourth;
+
     public
-    Tuple2(T1 first, T2 second) {
+    Tuple4(T1 first, T2 second, T3 third, T4 fourth) {
         this.first  = first;
         this.second = second;
+        this.third  = third;
+		this.fourth = fourth;
     }
 
     @Override public boolean
     equals(@Nullable Object obj) {
-    	if (!(obj instanceof Tuple2)) return false;
-    	@SuppressWarnings("unchecked") Tuple2<T1, T2> that = (Tuple2<T1, T2>) obj;
-    	return ObjectUtil.equals(this.first, that.first) && ObjectUtil.equals(this.second, that.second);
+    	if (!(obj instanceof Tuple4)) return false;
+    	@SuppressWarnings("unchecked") Tuple4<T1, T2, T3, T4> that = (Tuple4<T1, T2, T3, T4>) obj;
+    	return (
+			ObjectUtil.equals(this.first, that.first)
+			&& ObjectUtil.equals(this.second, that.second)
+			&& ObjectUtil.equals(this.third, that.third)
+			&& ObjectUtil.equals(this.fourth, that.fourth)
+		);
     }
 
     @Override public int
-    hashCode() { return ObjectUtil.hashCode(this.first) + ObjectUtil.hashCode(this.second); }
+    hashCode() {
+    	return (
+			ObjectUtil.hashCode(this.first)
+			+ ObjectUtil.hashCode(this.second)
+			+ ObjectUtil.hashCode(this.third)
+			+ ObjectUtil.hashCode(this.fourth)
+		);
+	}
 
-	@Override public String
+    @Override public String
     toString() {
-        return this.first + ":" + this.second;
+        return this.first + ":" + this.second + ":" + this.third + ":" + this.fourth;
     }
 }
