bb125f7ac43d318497da19bb0140c174aa9c0b60
Arno Unkrig <aunkrig@users.noreply.github.com>
2022-06-17 12:02:09 +0200

    Added tests for "MapUtil.lazyMap()".

--- commons-util/src/test/java/test/collections/MapUtilTest.java
+++ commons-util/src/test/java/test/collections/MapUtilTest.java
@@ -28,13 +28,22 @@
 
 package test.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import de.unkrig.commons.lang.protocol.Function;
 import de.unkrig.commons.util.collections.MapUtil;
+import de.unkrig.commons.util.collections.Sets;
 
 public
 class MapUtilTest {
@@ -243,5 +252,100 @@ class MapUtilTest {
         } catch (IllegalArgumentException iae) {
             ;
         }
+    }
+
+    @Test public void
+    testLazyMap() {
+        List<String> out = new ArrayList<>();
+        Function<Integer, String> vg1 = MapUtilTest.func("alpha", out);
+        Function<Integer, String> vg2 = MapUtilTest.func("beta", out);
+        Function<Integer, String> vg3 = MapUtilTest.func("gamma", out);
+
+        Map<Integer, Function<? super Integer, ? extends String>> valueGetters = new TreeMap<>();
+        valueGetters.put(1, vg1);
+        valueGetters.put(2, vg2);
+        valueGetters.put(3, vg3);
+        Map<Integer, String> lm = MapUtil.lazyMap(valueGetters, (Integer) 77);
+        Assert.assertEquals(Arrays.asList(), out);
+
+        // get()
+        Assert.assertEquals("77beta", lm.get(2));
+        Assert.assertEquals(Arrays.asList("77beta"), out);
+
+        // containsKey()
+        Assert.assertTrue(lm.containsKey(3));
+        Assert.assertEquals(Arrays.asList("77beta"), out);
+
+        // containsValue(nonexisting key)
+        out.clear();
+        Assert.assertFalse(lm.containsValue("xxx"));
+        Assert.assertEquals(Arrays.asList("77alpha", "77beta", "77gamma"), out);
+
+        // containsValue(existing key)
+        out.clear();
+        Assert.assertTrue(lm.containsValue("77beta"));
+        Assert.assertEquals(Arrays.asList("77alpha", "77beta"), out);
+
+        // keySet()
+        out.clear();
+        Assert.assertEquals(Sets.of(1, 2, 3), lm.keySet());
+        Assert.assertTrue(out.isEmpty());
+
+        // values().contains(existing value)
+        out.clear();
+        {
+            Collection<String> values = lm.values();
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertTrue(values.contains("77beta"));
+            Assert.assertEquals(Arrays.asList("77alpha", "77beta"), out);
+        }
+
+        // values().contains(nonexisting value)
+        out.clear();
+        {
+            Collection<String> values = lm.values();
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertFalse(values.contains("xxx"));
+            Assert.assertEquals(Arrays.asList("77alpha", "77beta", "77gamma"), out);
+        }
+
+        // values().iterator().hasNext(), .next()
+        out.clear();
+        {
+            Collection<String> values = lm.values();
+            Assert.assertEquals(Arrays.asList(), out);
+            Iterator<String> it = values.iterator();
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertTrue(it.hasNext());
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertEquals("77alpha", it.next());
+            Assert.assertEquals(Arrays.asList("77alpha"), out);
+        }
+
+        // entrySet().iterator().hasNext(), .next().getKey(), .getValue()
+        out.clear();
+        {
+            Collection<Entry<Integer, String>> entries = lm.entrySet();
+            Assert.assertEquals(Arrays.asList(), out);
+            Iterator<Entry<Integer, String>> it = entries.iterator();
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertTrue(it.hasNext());
+            Assert.assertEquals(Arrays.asList(), out);
+            Entry<Integer, String> e = it.next();
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertEquals(1, e.getKey().intValue());
+            Assert.assertEquals(Arrays.asList(), out);
+            Assert.assertEquals("77alpha", e.getValue());
+            Assert.assertEquals(Arrays.asList("77alpha"), out);
+        }
+    }
+
+    private static Function<Integer, String>
+    func(String s, Collection<String> out) {
+        return in -> {
+            String result = in + s;
+            out.add(result);
+            return result;
+        };
     }
 }
